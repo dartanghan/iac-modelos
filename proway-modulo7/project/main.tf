cf77b3382281342307ceb120886820c996525838
Dartanghan Vani <dartanghan.vani@recuperi.com.br>
2025-03-27 20:50:49 -0300

    fix: Ajuste em variable

--- proway-modulo7/project/main.tf
+++ proway-modulo7/project/main.tf
@@ -1,7 +1,6 @@
 module "vpc" {
     source = "./modules/vpc"
     project_name = var.project_name
-    project_region = var.project_region
 }
 module "loadbalancer" {
     source = "./modules/loadbalancer"
