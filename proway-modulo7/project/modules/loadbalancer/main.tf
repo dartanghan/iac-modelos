afdb66729ac011bac527b5f0afac96ea6370446c
Dartanghan Vani <dartanghan.vani@recuperi.com.br>
2025-03-27 20:55:10 -0300

    fix: Ajuste em variable

--- proway-modulo7/project/modules/loadbalancer/main.tf
+++ proway-modulo7/project/modules/loadbalancer/main.tf
@@ -7,6 +7,36 @@ data "aws_ami" "imagem_ec2" {
     }
 }
 
+
+resource "aws_security_group" "dart_nginx_sg" {
+    vpc_id = aws_vpc.dart_vpc.id
+    name = "dart_nginx_sg"
+    tags = {
+      Name = "dart-nginx_sg"
+    }
+}
+
+resource "aws_vpc_security_group_egress_rule" "dart_egress_sg_rule" {
+  security_group_id = aws_security_group.dart_nginx_sg.id
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
+} 
+
+resource "aws_vpc_security_group_ingress_rule" "dart_ingress_80_sg_rule" {
+  security_group_id = aws_security_group.dart_nginx_sg.id
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port   = 80
+  to_port     = 80
+}
+resource "aws_vpc_security_group_ingress_rule" "dart_ingress_22_sg_rule" {
+  security_group_id = aws_security_group.dart_nginx_sg.id
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port   = 22
+  to_port     = 22
+}
+
 resource "aws_network_interface" "dart_nginx_ei" {
   subnet_id = aws_subnet.sn_pub01.id
   tags = {
--- proway-modulo7/project/modules/vpc/main.tf
+++ proway-modulo7/project/modules/vpc/main.tf
@@ -38,32 +38,4 @@ resource "aws_subnet" "sn_pub02" {
   }
 }
 
-resource "aws_security_group" "dart_nginx_sg" {
-    vpc_id = aws_vpc.dart_vpc.id
-    name = "dart_nginx_sg"
-    tags = {
-      Name = "dart-nginx_sg"
-    }
-}
-
-resource "aws_vpc_security_group_egress_rule" "dart_egress_sg_rule" {
-  security_group_id = aws_security_group.dart_nginx_sg.id
-  cidr_ipv4   = "0.0.0.0/0"
-  ip_protocol = "-1"
-} 
-
-resource "aws_vpc_security_group_ingress_rule" "dart_ingress_80_sg_rule" {
-  security_group_id = aws_security_group.dart_nginx_sg.id
-  cidr_ipv4   = "0.0.0.0/0"
-  ip_protocol = "tcp"
-  from_port   = 80
-  to_port     = 80
-}
-resource "aws_vpc_security_group_ingress_rule" "dart_ingress_22_sg_rule" {
-  security_group_id = aws_security_group.dart_nginx_sg.id
-  cidr_ipv4   = "0.0.0.0/0"
-  ip_protocol = "tcp"
-  from_port   = 22
-  to_port     = 22
-}
 
